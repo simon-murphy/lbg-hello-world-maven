c95c53d95d4441f4a79ed06cc5829123dfdb320c
Simon M <simon@lbg.comgit config --global user.email simon@lbg.com>
2024-03-05 14:54:43 +0000

    Try again

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -10,9 +10,9 @@ public class App
     {
         System.out.println(sayHello());
         System.out.println("| | | | ___| | | ___   \ \      / /__  _ __| | __| | |");
-        System.out.println("| |_| |/ _ \ | |/ _ \   \ \ /\ / / _ \| '__| |/ _` | |");
+        System.out.println("| |_| |/ _ \ | |/ _ \   \ \ /\ / / _ \| __| |/ _ | |");
         System.out.println("|  _  |  __/ | | (_) |   \ V  V / (_) | |  | | (_| |_|");
-        System.out.println("|_| |_|\___|_|_|\___/     \_/\_/ \___/|_|  |_|\__,_(_)");
+        System.out.println("|_| |_|\___|_|_|\___/     \_/\_/ \___/|_|  |_|\____");
         System.out.println(sayGoodbye());
     }
 
