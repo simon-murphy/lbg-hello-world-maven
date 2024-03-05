e099225ca4eb95c79cd8d0220a3d4f29c902a86e
Simon M <simon@lbg.comgit config --global user.email simon@lbg.com>
2024-03-05 15:01:42 +0000

    Revert

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -8,33 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-       System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("            /~~~~~~~~\\                           _");
-        System.out.println("    ##\\__/ @)      ~~~~~~~~\\                     \\ \\ ) )");
-        System.out.println("    |              /~~\\~~~~~                ((    |  \\");
-        System.out.println("     \\    /           |                          /   |");
-        System.out.println("      (~~~   /         \\____________/~~~~~~~~~~~~   /");
-        System.out.println("       ~~~~|~                                     /");
-        System.out.println("           :                                      |");
-        System.out.println("            \\                                     |");
-        System.out.println("            |                               /      \\");
-        System.out.println("             \\  \\_         :         \\     /~~~\\    |");
-        System.out.println("             /   :~~~~~|   :~~~~~~~~~~|   :     :   :");
-        System.out.println("            /    :    /    :         /    :    /    :");
-        System.out.println("        (~~~     )(~~~     )     (~~~     )(~~~     )");
-        System.out.println("         ~~~~~~~~  ~~~~~~~~       ~~~~~~~~  ~~~~~~~~");
-        System.out.println("          STOMP     STOMP          STOMP     STOMP");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
+        System.out.println(sayHello());
+        System.out.println(sayHelloToSomeone("Simon M"));
+        System.out.println(sayHelloToSomeone("Gareth S"));
+        System.out.println(sayHelloToSomeone("Peter"));
+        System.out.println(sayHelloToSomeone("Donald"));
+        System.out.println(sayGoodbye());
     }
 
     public static String sayHello(){
